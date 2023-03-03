08caa532565bf7be9d9ac0dd735539d066bc26f0
kenny <kenneyg50@gmail.com>
2023-03-03 09:31:09 +0100

    frfr

--- src/views/terms/terms.jsx
+++ src/views/terms/terms.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
+import React from "react";
+// import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "./terms.css"
 
